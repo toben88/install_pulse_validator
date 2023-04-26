c47c677ad9c7793ebb59cc444e9d5d9d860c5d42
toben88 <tobwn@hotmail.com>
2023-04-25 21:27:53 -0500

    Update install_pulse_val.sh

--- install_pulse_val.sh
+++ install_pulse_val.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# Open three new terminal windows and execute the specified commands in each
+# Create a new tmux session named 'val'
+tmux new-session -d -s val
 
-# Open the first terminal window and execute the first command
-gnome-terminal --tab --title="Install" -- bash -c "git clone https://github.com/toben88/install_pulse_validator && cd install_pulse_validator && chmod +x setup_pulse_node.sh && sudo ./setup_pulse_node.sh; exec bash; exec bash"
+# Split the window vertically
+tmux split-window -h -t val
 
-# Open the 2nd terminal window and execute df -h
-gnome-terminal --tab --title="DiskUsage" -- bash -c "watch df -H; exec bash"
+# Select the first pane and split it vertically
+tmux split-window -v -t val:0.1
 
-# Open the 3rd terminal window and execute the second command
-gnome-terminal --tab --title="Instructions" -- bash -c "echo "wait for everything to stop and then type in exit 3 times to continue"; exec bash"
+# Evenly distribute the pane sizes
+#tmux select-layout -t val tiled
+
+# Send the commands to the respective panes
+tmux send-keys -t val:0.0 'git clone https://github.com/toben88/install_pulse_validator && cd install_pulse_validator && chmod +x setup_pulse_node.sh && sudo ./setup_pulse_node.sh; exec bash; exec bash' Enter
+tmux send-keys -t val:0.1 'df -h' Enter
+tmux send-keys -t val:0.2 'echo "┌─────────────────────────────────────────────────────────┐" && echo "│          Please enter the sudo password on the left to  │" && echo "│ begin installing 3 items for the Pulsechain Testnet v4  │" &&>
+
+
+# Select the first pane (0.0)
+tmux select-pane -t val:0.0
+
+# Attach to the tmux session
+tmux attach-session -t val:0.0
+
+echo "If you installed everything you can now view the action by running the following command from your blochcain directory"
+echo "default would be ./blockchain/log_viewer.sh" or
+echo "default would be ./blockchain/tmux_logviewer.sh"
+
 
 
