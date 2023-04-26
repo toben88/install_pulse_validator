97ec06063436841a2c4f0ce286ad2973af0f7fa5
toben88 <tobwn@hotmail.com>
2023-04-25 21:42:29 -0500

    Update install_pulse_val.sh

--- install_pulse_val.sh
+++ install_pulse_val.sh
@@ -4,18 +4,28 @@
 tmux new-session -d -s val
 
 # Split the window vertically
-tmux split-window -h -t val
+tmux split-window -v -t val
 
-# Select the first pane and split it vertically
-tmux split-window -v -t val:0.1
+# Select the 2ndt pane and split it horizontally
+tmux split-window -h -t val:0.1
 
 # Evenly distribute the pane sizes
 #tmux select-layout -t val tiled
 
 # Send the commands to the respective panes
 tmux send-keys -t val:0.0 'git clone https://github.com/toben88/install_pulse_validator && cd install_pulse_validator && chmod +x setup_pulse_node.sh && sudo ./setup_pulse_node.sh; exec bash; exec bash' Enter
-tmux send-keys -t val:0.1 'df -h' Enter
-tmux send-keys -t val:0.2 'echo "┌─────────────────────────────────────────────────────────┐" && echo "│          Please enter the sudo password on the left to  │" && echo "│ begin installing 3 items for the Pulsechain Testnet v4  │" &&>
+tmux send-keys -t val:0.1 'sudo df -h' Enter
+tmux send-keys -t val:0.2 'echo "┌─────────────────────────────────────────────────────────┐" ; \
+echo "│ On the left we are now installing 3 items for the       │" ; \
+echo "│ Pulsechain testnet v4 validator                         │" ;  \ 
+echo "│ 1. Execution client geth                                |" ;  \
+echo "| 2. Beacon client lighhouse                              │" ;  \
+echo "│ 3. Validator client                                     │" ;  \
+echo "│                                                         │" ;  \
+echo "│                                                         │" ;  \
+echo "│ It is strongly recommended that you review the script   │" ;  \
+echo "│ and understand its workings before proceeding.          │" ;  \
+echo "└─────────────────────────────────────────────────────────┘" ' Enter
 
 
 # Select the first pane (0.0)
@@ -25,8 +35,5 @@ tmux select-pane -t val:0.0
 tmux attach-session -t val:0.0
 
 echo "If you installed everything you can now view the action by running the following command from your blochcain directory"
-echo "default would be ./blockchain/log_viewer.sh" or
-echo "default would be ./blockchain/tmux_logviewer.sh"
-
-
-
+echo "default would be ./blockchain/log_viewer.sh" or 
+echo "default would be ./blockchain/tmux_logviewer.sh" 
