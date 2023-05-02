8b9ff87b76f9c92f12335791dbf8e2bac81b60f6
toben88 <tobwn@hotmail.com>
2023-05-02 17:21:34 -0500

    Update tmux_logviewer.sh

--- tmux_logviewer.sh
+++ tmux_logviewer.sh
@@ -19,7 +19,7 @@ tmux select-layout -t logs tiled
 tmux send-keys -t logs:0.0 'sudo docker logs -f execution' Enter
 tmux send-keys -t logs:0.1 'sudo docker logs -f validator' Enter
 tmux send-keys -t logs:0.2 'sudo docker logs -f beacon' Enter
-tmux send-keys -t logs:0.3 'sudo df -H && echo "If successful all 3 other panels should show activity. Wait until everything is fully synced to upload your .json file to the pulsechain validator website to become a validator. When everything is fully synced (This can be 24-48 hours) go to https://launchpad.v4.testnet.pulsechain.com/ and follow the instructions to deposit 32M Pulse' Enter
+tmux send-keys -t logs:0.3 'sudo df -H && echo "If successful all 3 other panels should show activity. To change panels just type <CTRL B> and press the arrow in the direction you want to change panels. Wait until everything is fully synced to upload your .json file to the pulsechain validator website to become a validator. When everything is fully synced (This can be 24-48 hours) go to https://launchpad.v4.testnet.pulsechain.com/ and follow the instructions to deposit 32M Pulse' Enter
 
 # Select the first pane (0.0)
 tmux select-pane -t logs:0.0
